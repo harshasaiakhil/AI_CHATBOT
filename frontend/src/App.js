f5f406349e4e3c9c24098517a64ae453b12b8b51
harshasaiakhil <harshasaiakhilb@gmail.com>
2026-04-10 23:08:10 +0530

    Changed model to openchat

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,7 +16,7 @@ function App() {
     setMessage("");
 
     try {
-      const res = await axios.post("http://127.0.0.1:8000/chat", {
+      const res = await axios.post("https://ai-chatbot-07vk.onrender.com/chat", {
         text: message,
       });
 
